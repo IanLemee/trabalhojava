922d9875b965120a3178cf20c7f529837cdc41b0
Ian Leme <130516228+IanLemee@users.noreply.github.com>
2023-04-30 20:04:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 var botaoSortear;
 var numeroSorteado;
 
+const handlePhone = (event) => {
+  let input = event.target
+  input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+  if (!value) return ""
+  value = value.replace(/\D/g,'')
+  value = value.replace(/(\d{2})(\d)/,"($1) $2")
+  value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+  return value
+}
+
 function sortearNumero(n){
   return Math.ceil(Math.random()*n)
 }
